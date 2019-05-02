aedf28f721780abd8e692cd72bac345f51d0bfda
nataliavalentin <nataliaferreiravalentin@gmail.com>
2019-05-02 11:46:39 -0300

    Estudos de lojinha

---
+++ loja/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from loja.models import Pedido, Produto
+
+# Register your models here.
+
+
+admin.site.register(Pedido)
+admin.site.register(Produto)
---
+++ loja/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from loja.forms import PedidoForm
+
+# Create your views here.
+
+def fazer_pedido(request):
+    formulario = PedidoForm(request.POST or None)
+    msg = ''
+    if formulario.is_valid():
+        formulario.save()
+        formulario = PedidoForm()
+        msg = 'Pedido realizado com sucesso'
+
+    contexto = {
+        'form' : formulario,
+        'msg' : msg
+    }
+
+
+
+    return render (request, 'index.html', contexto)
